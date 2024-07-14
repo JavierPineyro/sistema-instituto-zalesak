145a49ef7e1e875aa884cad854d84887692ba1e8
JavierPineyro <javidunam22@gmail.com>
2024-07-14 17:01:08 -0300

    Config eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,10 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
   "rules": {
+    "@typescript-eslint/prefer-optional-chain": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-assignment":"off",
+    "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
