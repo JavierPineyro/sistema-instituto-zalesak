86298c41b915c6ace5dfd2367885837d21b95990
JavierPineyro <javidunam22@gmail.com>
2024-07-17 20:47:57 -0300

    Add register form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,45 @@
-import { signIn } from "auth"
+import loginAction from "~/server/actions/login"
+import registerAction from "~/server/actions/register"
 
 export default function LoginPage() {
-  return <form
-    action={async (formData) => {
-      "use server"
-      await signIn("credentials", formData)
-    }}
-  >
-    <label>
-      Email
-      <input name="email" type="email" />
-    </label>
-    <label>
-      Password
-      <input name="password" type="password" />
-    </label>
-    <button>Sign In</button>
-  </form>
+  return (
+    <section>
+      <div>
+        <h2>Login</h2>
+        <form
+          action={loginAction}
+        >
+          <label>
+            Email
+            <input name="email" type="email" />
+          </label>
+          <label>
+            Password
+            <input name="password" type="password" />
+          </label>
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div>
+        <h2>Register</h2>
+        <form
+          action={registerAction}
+        >
+          <label>
+            Name
+            <input name="name" type="text" />
+          </label>
+          <label>
+            Email
+            <input name="email" type="email" />
+          </label>
+          <label>
+            Password
+            <input name="password" type="password" />
+          </label>
+          <button>Sign Up</button>
+        </form>
+      </div>
+    </section>
+  )
 }
