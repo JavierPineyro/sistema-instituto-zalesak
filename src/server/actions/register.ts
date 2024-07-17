3decf344dacaa757fa5b75a5d9c5199a57aaf37f
JavierPineyro <javidunam22@gmail.com>
2024-07-17 20:59:53 -0300

    AddAdd hash password in register action

--- src/server/actions/register.ts
+++ src/server/actions/register.ts
@@ -4,13 +4,22 @@ import { db } from "~/server/db";
 import { users } from "../db/schema";
 import { loginSchema } from "~/lib/validations/login.schema";
 import { ZodError } from "zod";
+import bcrypt from "bcryptjs"
 
 export default async function register(formData: FormData) {
   let err = null
 
   try {
-    const {name, email, password} = loginSchema.parse(formData)
+    // parse data
+    const data = loginSchema.parse(formData)
+
+    // hash password for safety
+    const password = await bcrypt.hash(data.password, 10)
+    const {name, email} = data
+
+    // insert hashed pass and user into db
     await db.insert(users).values({name, email, password})
+
   } catch (error) {
     console.log("Error that throws at Register Actions --->",error)
     if (error instanceof ZodError) {
