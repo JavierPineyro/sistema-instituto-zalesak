7412ff1ec6a5d7145588cf0b7bf0c1e4ea64fc22
JavierPineyro <javidunam22@gmail.com>
2024-07-20 23:50:55 -0300

    reove: ussles imports

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,6 @@
 import { auth } from "auth"
 import { redirect } from "next/navigation";
 import LoginForm from "~/components/auth/login-form";
-import loginAction from "~/server/actions/login"
 
 export default async function LoginPage() {
 
