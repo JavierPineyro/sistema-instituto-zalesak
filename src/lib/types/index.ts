07f1d1e96e91fc8514206950e4b2e709d67d24f9
JavierPineyro <javidunam22@gmail.com>
2024-07-20 23:50:26 -0300

    Add enum of messages

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,3 +1,10 @@
 export enum ServiceProvider {
   CREDENTIALS = "credentials"
 }
+
+export enum ValidationMessage{
+  WRONG_EMAIL_OR_PASSWORD = "Email o contraseña incorrecta",
+  UNEXPECTED_ERROR = "Ocurrió un error inesperado",
+  SUCCESSFUL_LOGIN = "Se inició sesión correctamente",
+  BAD_CREDENTIALS = "Credenciales no válidas"
+}
