790598c2a1ad63738f69dedf3d3c427a4cb7e5cf
JavierPineyro <javidunam22@gmail.com>
2024-07-25 19:58:37 -0300

    Add asmin icon for dashboard

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -99,7 +99,7 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+            <AvatarImage src="/admin.png" alt="@shadcn" />
             <AvatarFallback>Administrador</AvatarFallback>
           </Avatar>
         </Button>
