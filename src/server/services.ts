4a040d9f93f1ad918a6aacaa0c43107f4b39f9e8
JavierPineyro <javidunam22@gmail.com>
2024-08-05 00:57:36 -0300

    Add service repository to interact with DB

---
+++ src/server/services.ts
@@ -0,0 +1,34 @@
+import { mockAlumn } from "~/components/alumnos/tables/data";
+import { Alumn } from "~/lib/types";
+
+export const service = {
+  alumnos: {
+    list: async () => {
+      return mockAlumn;
+    },
+    getById: async (id: number): Promise<Alumn | null> => {
+      return new Promise((resolve, reject) => {
+        const alumn = mockAlumn.find((a) => a.id === id);
+        if (alumn) {
+          resolve(alumn);
+        } else {
+          reject(new Error("Alumn not found"));
+        }
+      });
+    },
+    save: async (alumn: Alumn) => {
+      mockAlumn.push(alumn);
+      return alumn;
+    },
+    update: async (alumn: Alumn) => {
+      const index = mockAlumn.findIndex((a) => a.id === alumn.id);
+      mockAlumn[index] = alumn;
+      return alumn;
+    },
+    delete: async (id: number) => {
+      const index = mockAlumn.findIndex((a) => a.id === id);
+      mockAlumn.splice(index, 1);
+      return true;
+    },
+  },
+};
