ecbd175cdaa8091baf691f61e11dfc0cab6dcd27
JavierPineyro <javidunam22@gmail.com>
2024-08-06 01:42:12 -0300

    Chane icon

--- src/components/alumnos/profile-sidebar.tsx
+++ src/components/alumnos/profile-sidebar.tsx
@@ -26,8 +26,8 @@ export function ProfileSidebar({ className, id }: SidebarProps) {
                 strokeLinejoin="round"
                 className="mr-2 h-4 w-4"
               >
-                <circle cx="12" cy="12" r="10" />
-                <polygon points="10 8 16 12 10 16 10 8" />
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
               </svg>
               Información Personal
             </Button>
@@ -60,11 +60,10 @@ export function ProfileSidebar({ className, id }: SidebarProps) {
                 strokeLinejoin="round"
                 className="mr-2 h-4 w-4"
               >
-                <path d="M4.9 19.1C1 15.2 1 8.8 4.9 4.9" />
-                <path d="M7.8 16.2c-2.3-2.3-2.3-6.1 0-8.5" />
-                <circle cx="12" cy="12" r="2" />
-                <path d="M16.2 7.8c2.3 2.3 2.3 6.1 0 8.5" />
-                <path d="M19.1 4.9C23 8.8 23 15.1 19.1 19" />
+                <path d="m16 6 4 14" />
+                <path d="M12 6v14" />
+                <path d="M8 8v12" />
+                <path d="M4 4v16" />
               </svg>
               Recibos
             </Button>
