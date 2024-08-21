dca8e28affe7d4b7bc0e594e294cea00ebd5b8b7
JavierPineyro <javidunam22@gmail.com>
2024-08-20 23:35:14 -0300

    Add coments and update readme

--- src/server/actions/alumnos/create-action.tsx
+++ src/server/actions/alumnos/create-action.tsx
@@ -12,7 +12,10 @@ export default async function createAlumnAction(alumn: NewAlumn) {
     success: true,
     message: ''
   }
-
+  // Verificar si alguno de los datos llega vacio (como el cinturon o otros) 
+  // y enviar un mensaje de error para que no solo diga Error creando alumno
+  // y diga que el error es porque no se completo el dato que falta
+  // enviar un mensaje de error diferente por cada campo que falte y que zod no lo reconozca, como los SELECT o eso, usar el parse y no safeParse si es necesario.
   try {
     const { data, success } = AlumnCreateSchema.safeParse(alumn)
     if (!success) {
