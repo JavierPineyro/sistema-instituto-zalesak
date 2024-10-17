3ce27a54fce3a5aa5e3e5267915d23244305b06c
Javier Piñeyro <dariojavierpineyro@gmail.com>
2024-10-17 17:37:15 -0300

    Change arrow icon size

--- src/components/go-back.tsx
+++ src/components/go-back.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function GoBack({ path = "/admin/alumnos" }: { path: string }) {
   return (
     <Link href={path}>
-      <ArrowLeft className="inline mr-2 size-2" />
+      <ArrowLeft className="mr-2 inline size-3" />
       Volver
     </Link>
-  )
+  );
 }
